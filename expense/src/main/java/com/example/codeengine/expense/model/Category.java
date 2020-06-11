00aea57a70d22b1bd229b0f370cc9c0f68f6da67
jonah <jonah@DESKTOP-E0IGJM9>
2020-06-11 21:59:14 +0200

    Started.

---
+++ expense/src/main/java/com/example/codeengine/expense/model/Category.java
@@ -0,0 +1,32 @@
+package com.example.codeengine.expense.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name="category")
+public class Category {
+
+	@Id
+	private Long id;
+
+
+
+	@NonNull
+	private String name;//Travel..etc
+
+	/*
+	 * @ManyToOne(cascade = CascadeType.PERSIST) private User user;
+	 */
+}
