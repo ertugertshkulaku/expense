e622524f67575edfc703cbf0cbd4040b08528395
Eri <Eri@Ertugert-Pc>
2020-06-12 16:02:51 +0200

    .

--- expense/bin/src/main/resources/Data.sql
+++ expense/bin/src/main/resources/Data.sql
@@ -2,9 +2,9 @@ insert into	user values(1, 'Simak', 'Codeenginesimak@gmail.com');
 insert into	user values(2, 'John', 'Codeenginejohn@gmail.com');
 insert into	user values(3, 'Adam', 'Codeengineadam@gmail.com');
 
-insert into category values(1, 'Travel');
-insert into category values(2, 'Auto Loan');
-insert into category values(3, 'Student Loan');
+insert into category values(1, 'Travel', 1);
+insert into category values(2, 'Auto Loan', 2);
+insert into category values(3, 'Student Loan', 3);
 
 insert into expense values(100, 'New York', '2019-06-16T17:00:00.000Z',1,1);
 insert into expense values(101, 'Ford Mustang', '2019-06-15T17:00:00.000Z',2,2);
