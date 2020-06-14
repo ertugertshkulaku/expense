93ebce35ca35623fe796571b80fea2a1b1f80ac8
ertugertshkulaku <ertugert.shkulaku1@gmail.com>
2020-06-14 19:10:03 +0200

    Add expenses in a table

--- app/src/Expenses.js
+++ app/src/Expenses.js
@@ -3,22 +3,63 @@ import AppNav from './AppNav';
 import DatePicker from "react-datepicker";
 import './App.css';
 import "react-datepicker/dist/react-datepicker.css";
-import { Container, FormGroup, Button,  Label, Form , Input} from 'reactstrap';
+import { Container, FormGroup, Button,  Label, Form , Input, Table} from 'reactstrap';
 import {Link} from 'react-router-dom';
+import Moment from 'react-moment';
 
 
 class Expenses extends Component {
-    state = { 
-        date : new Date(),
-        isLoading : true,
-        expenses : [],
-        Categories : []
+
+    emptyItem = {
+        
+        expensedate : new Date(),
+        description : '',
+        id: 104,
+        location : '',
+        categories : [1, 'Travel']
+
     }
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+           
+            isLoading : false,
+            Expenses : [],
+            Categories : [],
+            date : new Date(),
+            item : this.emptyItem
+
+        }
+    }
+
+
+    async remove(id){
+        await fetch(`/api/expenses/${id}` , {
+          method: 'DELETE' ,
+          headers : {
+            'Accept' : 'application/json',
+            'Content-Type' : 'application/json'
+          }
+
+        }).then(() => {
+          let updatedExpenses = [...this.state.Expenses].filter(i => i.id !== id);
+          this.setState({Expenses : updatedExpenses});
+        });
+
+    }
+
+    
       async  componentDidMount(){
           const response = await fetch('/api/categories');
           const body = await response.json();
-
           this.setState({Categories : body , isLoading : false});
+
+
+          const responseExp = await fetch('/api/expenses');
+          const bodyExp = await responseExp.json();
+          this.setState({Expenses : bodyExp , isLoading : false});
       }
 
 
@@ -26,7 +67,9 @@ class Expenses extends Component {
     render() { 
 
         const title = <h3>Add Expense</h3>
-        const  {Categories, isLoading} = this.state;
+        const  {Categories} = this.state;
+        const {Expenses, isLoading} = this.state;
+
         if(isLoading)
         return(<div>Loading..../</div>)
 
@@ -36,6 +79,21 @@ class Expenses extends Component {
                 {category.name}
                  </option>
             )
+
+            let rows=
+            Expenses.map( expense =>
+              <tr key={expense.id}>
+                <td>{expense.description}</td>
+                <td>{expense.location}</td>
+                <td><Moment date={expense.expensedate} format="YYYY/MM/DD"/></td>
+                <td>{expense.category.name}</td>
+                <td><Button size="sm" color="danger" onClick={() => this.remove(expense.id)}>Delete</Button></td>
+
+              </tr>
+
+
+            )
+
         
         return ( <div><AppNav/>
 
@@ -51,7 +109,7 @@ class Expenses extends Component {
                         <select>
                         {optionList}
                         </select>
-                        <Input type="text" name="category" id="category" onChange={this.handleChange}/>
+                        
                     </FormGroup>
                     <FormGroup>
                         <Label for="city"> Date</Label>
@@ -73,10 +131,37 @@ class Expenses extends Component {
 
 
             </Container>
+            {''}
+            <Container>
+                <h3>Expense List</h3>
+                <Table className="mt-4">
+                    <thead>
+                        <tr>
+                            <th width="20%">Description</th>
+                            <th width="10%">Location</th>
+                            <th>Date</th>
+                            <th >Category</th>
+                            <th width="10%">Action</th>
+
+                        </tr>
+
+
+                    </thead>
+                    <tbody>
+                        {rows}
+                    </tbody>
 
 
 
-       </div> );
+
+                </Table>
+
+            </Container>
+
+
+
+       </div> 
+       );
     }
 }
  
